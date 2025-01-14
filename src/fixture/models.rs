0c966efb03f13d775fca888f9b9eb0e6843fe0bd
Andy Gallagher <fredex42@gmail.com>
2025-01-14 22:36:23 +0000

    fix timestamp in get /collections

--- src/fixture/models.rs
+++ src/fixture/models.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+//Note - use rfc2822 for last-modified and if-modified-since
 #[derive(Serialize, Deserialize, Debug)]
 pub enum CollectionKind {
     #[serde(rename="saved")]
@@ -18,7 +19,7 @@ pub struct CollectionResponse {
     pub id: String,
     #[serde(rename="collectionType")]
     pub collection_type:CollectionKind,
-    #[serde(rename="lastModified")]
+    #[serde(rename="lastModified",serialize_with="time::serde::rfc3339::serialize")]
     pub last_modified: time::OffsetDateTime   //also in header
 }
 
